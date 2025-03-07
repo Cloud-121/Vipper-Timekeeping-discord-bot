18c7f64655f9b872aeb9a13f243ed6da1b387c54
Cloud Hayes <Cloud@gproconnect.com>
2025-03-07 12:40:33 -0600

    Update

---
+++ README.md
@@ -0,0 +1,11 @@
+Hiya, This bot was developed by @Cloud-121. I designed this bot to help discord servers with users in multiple timezones to know each others time and be able to say there time simply without having to pull up a unix clock. 
+This bot is completely free and open source on my github [here](https://github.com/Cloud-121/Vipper-Timekeeping-discord-bot).
+A few commands you can use are:
+
+`/registertimezone [timezone or currenttime]` - Register your timezone
+
+`/whatsthetime [user]` - Show your current time in your registered timezone
+
+`/whatismytimezone` - Show your current registered timezone
+
+`/help` - Show this help message
--- main.py
+++ main.py
@@ -161,8 +161,8 @@ async def whatsthetime(interaction: discord.Interaction, user: Optional[discord.
 async def help(interaction: discord.Interaction):
     await interaction.response.send_message(
         """
-Hiya, This bot was developed by @Cloudfoxies. I designed this bot to help discord servers with users in multiple timezones to know each others time and be able to say there time simply without having to pull up a unix clock. 
-
+Hiya, This bot was developed by @Cloud-121. I designed this bot to help discord servers with users in multiple timezones to know each others time and be able to say there time simply without having to pull up a unix clock. 
+This bot is completely free and open source on my github [here](https://github.com/Cloud-121/Vipper-Timekeeping-discord-bot).
 A few commands you can use are:
 
 `/registertimezone [timezone or currenttime]` - Register your timezone
